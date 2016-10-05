c12e8ca9847917aee930167ebb84e3f6faf00f62
Malte Gruber <malteg@kth.se>
2016-10-05 11:43:18 +0200

    Gui Spike 2

---
+++ src/main/java/kth/id2007/project/GUI.java
@@ -0,0 +1,10 @@
+package kth.id2007.project;
+
+public class GUI {
+	
+	public static void main(String[] args) {
+		new StartFrame();
+		new StartFrame();
+		new StartFrame();
+	}
+}   
--- src/main/java/kth/id2007/project/StartFrame.java
+++ src/main/java/kth/id2007/project/StartFrame.java
@@ -1,13 +1,43 @@
 package kth.id2007.project;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
+
 /**
- * Hello world!
- *
- */
-public class App 
+ * 
+ * 
+ */  
+class StartFrame extends JFrame
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! MG" );
-    }
+	private MigLayout layout;
+	private JTextField userNameField,passwordField;
+	private JButton loginButton;
+	
+	public StartFrame(){
+		super("Test");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		layout=new MigLayout("wrap 2");
+		setLayout(layout);
+	
+		userNameField=new JTextField(25);
+		passwordField=new JTextField(25);
+		loginButton=new JButton("Log in");
+		
+		
+		add(new JLabel("Login"),"span 2");
+		add(new JLabel("Username"),"span 1");
+		add(userNameField,"span 1");
+		add(new JLabel("Password"),"span 1");
+		add(passwordField,"span 1");
+		add(loginButton,"span 1");
+		
+		pack();
+		setVisible(true);
+	}
+
 }
