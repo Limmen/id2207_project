b163e34d0a97f0b4a7de0cbb58b3f7dede2ac59e
limmen <kimham@kth.se>
2016-10-05 11:09:59 +0200

    initial commit

---
+++ src/main/java/kth/id2007/project/App.java
@@ -0,0 +1,13 @@
+package kth.id2007.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
