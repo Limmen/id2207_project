52f04598757468f4144b77ac67d967363f998d54
Malte Gruber <malteg@kth.se>
2016-10-07 10:18:05 +0200

    Added client record gui, class and test

--- src/main/java/kth/id2007/project/view/GUI.java
+++ src/main/java/kth/id2007/project/view/GUI.java
@@ -10,6 +10,7 @@ public class GUI {
 
     public GUI(){
         startFrame = new StartFrame(this);
+       	new ClientRecordFrame(this);
     }
 
     public static void main(String[] args) {
@@ -32,12 +33,41 @@ public class GUI {
          * @param e
          */
         public void actionPerformed(ActionEvent e) {
-            if(usernameField.getText().equals("test") && passwordField.getText().equals("test"))
-                JOptionPane.showInputDialog(null, "Login Success", "Login Success", JOptionPane.INFORMATION_MESSAGE);
-            else
-                JOptionPane.showInputDialog(null, "Login Fail", "Login Fail", JOptionPane.INFORMATION_MESSAGE);
+            if(usernameField.getText().equals("test") && passwordField.getText().equals("test")){
+                JOptionPane.showMessageDialog(null, "Login Success", "Login Success", JOptionPane.INFORMATION_MESSAGE);
+            	
+            }else
+                JOptionPane.showMessageDialog(null, "Login Fail", "Login Fail", JOptionPane.INFORMATION_MESSAGE);
         }
     }
+    
+    class ClientRecordListener implements ActionListener {
+    	private JTextField clientNameField;
+    	private JTextField clientEmailField;
+    	private JTextField clientPhoneNumberField;
+    	
+    	public ClientRecordListener(JTextField clientNameField, JTextField clientEmailField, JTextField clientPhoneNumberField) {
+			this.clientNameField=clientNameField;
+			this.clientEmailField=clientEmailField;
+			this.clientPhoneNumberField=clientPhoneNumberField;
+		}
+
+		public void actionPerformed(ActionEvent arg0) {
+			if((clientNameField.getText().length()>0)&&
+					(clientEmailField.getText().length()>0)&&
+					(clientPhoneNumberField.getText().length()>0)){
+				//Ok
+				System.out.println("Client Name "+clientNameField.getText()+" Email "+clientEmailField.getText()+" Phone "+clientPhoneNumberField.getText());
+				
+			}else{
+				
+                JOptionPane.showMessageDialog(null, "All fields need to be filled out.","Error", JOptionPane.INFORMATION_MESSAGE);			
+			}
+    	}
+
+    }
+
+
 
 
 }   
